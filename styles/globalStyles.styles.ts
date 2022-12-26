8ffc299c4d3f82d7604e6fb7933c2e03b94b3b0e
Nikita Babko <niki.babko@gmail.com>
2022-12-26 15:53:13 +0700

    style: add base font

--- styles/globalStyles.styles.ts
+++ styles/globalStyles.styles.ts
@@ -1,4 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 import '@nikitababko/reset-css';
+import { Roboto } from '@next/font/google';
 
-export const GlobalStyles = createGlobalStyle``;
+const roboto = Roboto({
+  weight: '400',
+  style: ['normal'],
+  subsets: ['latin'],
+});
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    font-family: ${roboto.style.fontFamily}, ui-monospace, Menlo, Monaco, 'Cascadia Mono', 'Segoe UI Mono',
+    'Roboto Mono', 'Oxygen Mono', 'Ubuntu Monospace', 'Source Code Pro',
+    'Fira Mono', 'Droid Sans Mono', 'Courier New', monospace;
+    font-weight: ${roboto.style.fontWeight};
+    font-style: ${roboto.style.fontStyle};
+  }
+`;
