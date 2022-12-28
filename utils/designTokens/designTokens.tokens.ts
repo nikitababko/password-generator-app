b3c2aa7ba030b2f7ad1047092de34ed451a5ee75
Nikita Babko <niki.babko@gmail.com>
2022-12-28 21:44:06 +0700

    chore(utils,styles): add design tokens 'ColorDesignTokens'

--- utils/designTokens/designTokens.tokens.ts
+++ utils/designTokens/designTokens.tokens.ts
@@ -3,3 +3,14 @@ export enum MediaBreakPointsDesignTokens {
   media768 = 768,
   media480 = 480,
 }
+
+export enum ColorDesignTokens {
+  BlueDark = '#157AF5',
+  BlueLight = '#E9F4FE',
+  BlackDark = '#222222',
+  BlackLight = '#2E3D54',
+  Gray = '#F0F4F7',
+  YellowDark = '#FFBB52',
+  YellowLight = '#FFECCF',
+  White = '#FFFFFF',
+}
