bba18c24d0e27a0eee3d075a7cca61a421e92132
Nikita Babko <niki.babko@gmail.com>
2023-01-05 18:56:21 +0700

    refactor: prettier --fix 'Layout' component

--- components/Layout/Layout.styles.ts
+++ components/Layout/Layout.styles.ts
@@ -2,5 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 100vh;
-  background: ${({ theme }) => theme.colors.backgroundColor};
+  background: ${({ theme }) => {
+    return theme.colors.backgroundColor;
+  }};
 `;
