ede73a35556c037dbe1f092ac8d315925cee6134
Nikita Babko <niki.babko@gmail.com>
2023-01-05 19:00:34 +0700

    style: add 'padding' to 'Layout' component

--- components/Layout/Layout.styles.ts
+++ components/Layout/Layout.styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 100vh;
+  padding: 36px 40px;
   background: ${({ theme }) => {
     return theme.colors.backgroundColor;
   }};
