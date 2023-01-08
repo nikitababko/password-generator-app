ec974024384233718d327af2ed1820ad5d82235e
Nikita Babko <niki.babko@gmail.com>
2023-01-08 20:26:28 +0700

    feat(components,ui): add ability to change language

--- components/Header/Header.component.tsx
+++ components/Header/Header.component.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { Title } from '../Title';
+import { SelectField } from '../SelectField';
 
 import { Container } from './Header.styles';
+import { data } from './Header.data';
 
 export const Header: React.FC = () => {
   return (
     <Container>
       <Title />
+      <SelectField data={data} />
     </Container>
   );
 };
---
+++ components/Header/Header.data.tsx
@@ -0,0 +1,187 @@
+import React from 'react';
+
+import { SelectFieldItemType } from '../SelectField/SelectField.types';
+import { Icon } from '../Icon';
+
+export const data: SelectFieldItemType[] = [
+  {
+    id: 1,
+    icon: (
+      <Icon
+        name="FlagUs"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'English',
+  },
+  {
+    id: 2,
+    icon: (
+      <Icon
+        name="FlagRu"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'Русский',
+  },
+  {
+    id: 3,
+    icon: (
+      <Icon
+        name="FlagCh"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: '中國人',
+  },
+  {
+    id: 4,
+    icon: (
+      <Icon
+        name="FlagKr"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: '한국인',
+  },
+  {
+    id: 5,
+    icon: (
+      <Icon
+        name="FlagJp"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: '日本語',
+  },
+  {
+    id: 6,
+    icon: (
+      <Icon
+        name="FlagFr"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'Français',
+  },
+  {
+    id: 7,
+    icon: (
+      <Icon
+        name="FlagDe"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'Deutsch',
+  },
+  {
+    id: 8,
+    icon: (
+      <Icon
+        name="FlagIn"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'Indonesia',
+  },
+  {
+    id: 9,
+    icon: (
+      <Icon
+        name="FlagPl"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'Polski',
+  },
+  {
+    id: 10,
+    icon: (
+      <Icon
+        name="FlagEs"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'Español',
+  },
+  {
+    id: 11,
+    icon: (
+      <Icon
+        name="FlagGe"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'ქართული',
+  },
+  {
+    id: 12,
+    icon: (
+      <Icon
+        name="FlagAe"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'عرب',
+  },
+  {
+    id: 13,
+    icon: (
+      <Icon
+        name="FlagAm"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'հայերեն',
+  },
+  {
+    id: 14,
+    icon: (
+      <Icon
+        name="FlagIn"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'हिन्दी',
+  },
+  {
+    id: 15,
+    icon: (
+      <Icon
+        name="FlagIt"
+        style={{
+          boxShadow: '0px 0px 5px 3px rgb(34 60 80 / 20%)',
+        }}
+      />
+    ),
+    label: 'Italiano',
+  },
+];
