d0dd9772c5142737878ec139d90f8611e8f14c16
Nikita Babko <niki.babko@gmail.com>
2023-01-25 22:39:01 +0700

    style(ui): fix styles
    * Remove 'height' from 'Container'
    * Add 'height' and 'padding' to 'Content'

--- components/SelectField/SelectField.styles.ts
+++ components/SelectField/SelectField.styles.ts
@@ -6,14 +6,16 @@ export const Container = styled.div`
   position: relative;
   width: 100%;
   max-width: 196px;
+  height: 55px;
 `;
 
 export const Content = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 0 16px;
   width: 100%;
-  padding: 12px 14px;
+  height: 100%;
   background-color: ${({ theme }) => {
     return theme.colors.inputBackgroundColor;
   }};
@@ -28,5 +30,7 @@ export const ChevronWrapper = styled.div<ChevronWrapperStylesType>`
   transform: ${({ isOpen }) => {
     return `rotate(${isOpen ? '180deg' : '0'}) `;
   }};
-  transition: ${({ theme }) => theme.transition()};
+  transition: ${({ theme }) => {
+    return theme.transition('transform');
+  }};
 `;
