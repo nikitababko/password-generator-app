66b8d569f3d21118c121ca48fb9de8cd3409c38c
Nikita Babko <niki.babko@gmail.com>
2023-02-03 13:35:30 +0700

    feat(components): add Top and Mid sections
    Top section: it's wrapper for form items;
    Mid section: add generate button, output field for
    password and copy button.

--- components/Form/Form.component.tsx
+++ components/Form/Form.component.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
 
-import { Container } from './Form.styles';
+import {
+  Border,
+  Container,
+  CopyButton,
+  GenerateButton,
+  GenerateButtonLabel,
+  Mid,
+  PasswordLabel,
+  PasswordWrapper,
+  Top,
+} from './Form.styles';
 import { FormItem } from './FormItem';
 import { formItemData } from './FormItem/FormItem.data';
+import { Icon } from '../Icon';
 
 export const Form: React.FC = () => {
   return (
     <Container>
-      {formItemData.map((formItem) => (
-        <FormItem key={formItem.id} item={formItem} />
-      ))}
+      <Top>
+        {formItemData.map((formItem) => (
+          <FormItem key={formItem.id} item={formItem} />
+        ))}
+      </Top>
+
+      <Border />
+
+      <Mid>
+        <GenerateButton>
+          <GenerateButtonLabel>
+            Generate button
+          </GenerateButtonLabel>
+          <Icon name="ArrowRepeat" width={31} height={32} />
+        </GenerateButton>
+
+        <PasswordWrapper>
+          <PasswordLabel>
+            {'Wl2_Xt3:Zp8}Wz4+Ih3!Hd6:Zq'}
+          </PasswordLabel>
+        </PasswordWrapper>
+
+        <CopyButton>
+          <Icon name="CopySelect" width={31} height={32} />
+        </CopyButton>
+      </Mid>
     </Container>
   );
 };
--- components/Form/Form.styles.ts
+++ components/Form/Form.styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
-import { ColorDesignTokens } from '../../utils/designTokens';
+
+import {
+  Animation,
+  ColorDesignTokens,
+  FontSize,
+} from '../../utils/designTokens';
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 14px;
   align-self: center;
   padding: 40px 124px;
   width: 988px;
@@ -14,4 +16,86 @@ export const Container = styled.div`
   }};
   border-radius: 12px;
   box-shadow: 0 15px 37px -3px ${ColorDesignTokens.Black}45;
+`;
+
+export const Top = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 14px;
+`;
+
+export const Border = styled.div`
+  margin: 30px 0;
+  width: 100%;
+  height: 2px;
+  background-color: ${({ theme }) => {
+    return theme.colors.borderColor;
+  }};
+`;
+
+export const Mid = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const GenerateButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 14px;
+  width: 240px;
+  height: 56px;
+  border-radius: 12px;
+  background-color: ${({ theme }) => {
+    return theme.colors.generateButtonBackgroundColor;
+  }};
+  cursor: pointer;
+  transition: box-shadow ${Animation};
+  border: none;
+
+  &:hover {
+    box-shadow: 0 0 16px 1px ${ColorDesignTokens.Black};
+  }
+`;
+
+export const GenerateButtonLabel = styled.p`
+  font-size: ${FontSize.sm};
+  line-height: 26px;
+  font-weight: 400;
+  color: ${ColorDesignTokens.White};
+`;
+
+export const PasswordWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 34px;
+  height: 56px;
+  background-color: ${({ theme }) => {
+    return theme.colors.formItemLeftColBackgroundColor;
+  }};
+  border-radius: 12px;
+`;
+
+export const PasswordLabel = styled.p`
+  background-color: ${({ theme }) => {
+    return theme.colors.formItemLeftColBackgroundColor;
+  }};
+  font-size: ${FontSize.sm};
+  line-height: 26px;
+  border-radius: 12px;
+`;
+
+export const CopyButton = styled.button`
+  width: 56px;
+  height: 56px;
+  border-radius: 12px;
+  background-color: ${ColorDesignTokens.Blue};
+  cursor: pointer;
+  transition: box-shadow ${Animation};
+  border: none;
+
+  &:hover {
+    box-shadow: 0 0 16px 1px ${ColorDesignTokens.Blue};
+  }
 `;
