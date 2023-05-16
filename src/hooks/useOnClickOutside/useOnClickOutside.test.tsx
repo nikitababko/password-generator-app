560d48f01883303bf91003167edf2c2f8bfa2d84
Nikita Babko <niki.babko@gmail.com>
2023-05-16 17:32:54 +0700

    test: add tests for hooks

---
+++ src/hooks/useOnClickOutside/useOnClickOutside.test.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
+import { useOnClickOutside } from './useOnClickOutside.hook';
+
+const TestComponent = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+  useOnClickOutside(ref, () => setIsOpen(false));
+
+  return (
+    <div>
+      <div ref={ref}>
+        <button onClick={() => setIsOpen(true)}>
+          Open
+        </button>
+
+        {isOpen && <div>Conditional block</div>}
+      </div>
+
+      <div>Close</div>
+    </div>
+  );
+};
+
+describe('useOnClickOutside', () => {
+  it('render', async () => {
+    render(<TestComponent />);
+
+    const Open = screen.getByText('Open');
+
+    expect(Open).toBeInTheDocument();
+
+    await userEvent.click(Open);
+
+    const ConditionalBlock = screen.getByText(
+      'Conditional block',
+    );
+
+    expect(ConditionalBlock).toBeInTheDocument();
+
+    const Close = screen.getByText('Close');
+
+    await userEvent.click(Close);
+
+    expect(ConditionalBlock).not.toBeInTheDocument();
+  });
+});
---
+++ src/hooks/useTranslate/useTranslate.test.tsx
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom';
+import { renderHook } from '@testing-library/react';
+
+import { useTranslate } from './useTranslate.hook';
+import { Translations } from './useTranslate.data';
+
+describe('useTranslate', () => {
+  it('render', async () => {
+    const { result } = renderHook(() => {
+      return useTranslate();
+    });
+
+    expect(result.current.passwordLength).toEqual(
+      Translations.en.passwordLength,
+    );
+  });
+});
