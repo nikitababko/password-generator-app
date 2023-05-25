4d70e2496589021d3ae579e747c53ee2bf5c1324
Nikita Babko <niki.babko@gmail.com>
2023-05-25 18:37:27 +0700

    chore: add package `ts-essentials` and use `DeepReadonly` for `Translations`

--- src/hooks/useTranslate/useTranslate.data.ts
+++ src/hooks/useTranslate/useTranslate.data.ts
@@ -1,181 +1,190 @@
+import type { DeepReadonly } from 'ts-essentials';
+
 import type { TranslationsType } from './useTranslate.types';
 
-export const Translations: TranslationsType = {
-  en: {
-    passwordLength: 'Password Length',
-    includeSymbols: 'Include Symbols',
-    includeNumbers: 'Include Numbers',
-    includeLowercaseCharacters:
-      'Include Lowercase Characters',
-    includeUppercaseCharacters:
-      'Include Uppercase Characters',
-    excludeSimilarCharacters: 'Exclude Similar Characters',
-    excludeAmbiguousCharacters:
-      'Exclude Ambiguous Characters',
-    generate: 'Generate',
-  },
-  ru: {
-    passwordLength: 'Длина пароля',
-    includeSymbols: 'Включить символы',
-    includeNumbers: 'Включить числа',
-    includeLowercaseCharacters: 'Включить строчные буквы',
-    includeUppercaseCharacters: 'Включить заглавные буквы',
-    excludeSimilarCharacters: 'Исключить похожие символы',
-    excludeAmbiguousCharacters:
-      'Исключить неоднозначные символы',
-    generate: 'Сгенерировать',
-  },
-  zh: {
-    passwordLength: '密碼長度',
-    includeSymbols: '包括符號',
-    includeNumbers: '包括數字',
-    includeLowercaseCharacters: '包括小寫字符',
-    includeUppercaseCharacters: '包括大寫字符',
-    excludeSimilarCharacters: '排除相似字符',
-    excludeAmbiguousCharacters: '排除歧義字符',
-    generate: '產生',
-  },
-  ko: {
-    passwordLength: '비밀번호 길이',
-    includeSymbols: '포함 기호',
-    includeNumbers: '포함 숫자',
-    includeLowercaseCharacters: '소문자 포함',
-    includeUppercaseCharacters: '대문자 포함',
-    excludeSimilarCharacters: '유사한 문자 제외',
-    excludeAmbiguousCharacters: '모호한 문자 제외',
-    generate: '생성하다',
-  },
-  ja: {
-    passwordLength: 'パスワードの長さ',
-    includeSymbols: '記号を含む',
-    includeNumbers: '数字を含む',
-    includeLowercaseCharacters: '小文字を含める',
-    includeUppercaseCharacters: '大文字を含む',
-    excludeSimilarCharacters: '類似文字を除外',
-    excludeAmbiguousCharacters: 'あいまいな文字を除外する',
-    generate: '生成',
-  },
-  fr: {
-    passwordLength: 'Longueur du mot de passe',
-    includeSymbols: 'Inclure des symboles',
-    includeNumbers: 'Inclure des nombres',
-    includeLowercaseCharacters:
-      'Inclure des caractères minuscules',
-    includeUppercaseCharacters:
-      'Inclure les caractères majuscules',
-    excludeSimilarCharacters:
-      'Exclure les caractères similaires',
-    excludeAmbiguousCharacters:
-      'Exclure les caractères ambigus',
-    generate: 'Générer',
-  },
-  de: {
-    passwordLength: 'Passwortlänge',
-    includeSymbols: 'Symbole einbeziehen',
-    includeNumbers: 'Zahlen einschließen',
-    includeLowercaseCharacters: 'Enthält Kleinbuchstaben',
-    includeUppercaseCharacters:
-      'Großbuchstaben einschließen',
-    excludeSimilarCharacters:
-      'Schließen Sie ähnliche Zeichen aus',
-    excludeAmbiguousCharacters:
-      'Schließen Sie mehrdeutige Zeichen aus',
-    generate: 'Generieren',
-  },
-  id: {
-    passwordLength: 'Panjang kata sandi',
-    includeSymbols: 'Sertakan simbol',
-    includeNumbers: 'Sertakan angka',
-    includeLowercaseCharacters:
-      'Termasuk karakter huruf kecil',
-    includeUppercaseCharacters:
-      'Termasuk karakter huruf besar',
-    excludeSimilarCharacters:
-      'Kecualikan karakter yang mirip',
-    excludeAmbiguousCharacters:
-      'Kecualikan karakter yang ambigu',
-    generate: 'Menghasilkan',
-  },
-  pl: {
-    passwordLength: 'Długość hasła',
-    includeSymbols: 'Dołącz symbole',
-    includeNumbers: 'Uwzględnij liczby',
-    includeLowercaseCharacters: 'Zawiera małe litery',
-    includeUppercaseCharacters: 'Zawiera wielkie litery',
-    excludeSimilarCharacters: 'Wyklucz podobne znaki',
-    excludeAmbiguousCharacters:
-      'Wyklucz niejednoznaczne znaki',
-    generate: 'Generować',
-  },
-  es: {
-    passwordLength: 'Longitud de la contraseña',
-    includeSymbols: 'Incluir símbolos',
-    includeNumbers: 'Incluir números',
-    includeLowercaseCharacters:
-      'Incluir caracteres en minúscula',
-    includeUppercaseCharacters:
-      'Incluir caracteres en mayúscula',
-    excludeSimilarCharacters:
-      'Excluir caracteres similares',
-    excludeAmbiguousCharacters:
-      'Excluir caracteres ambiguos',
-    generate: 'Generar',
-  },
-  ka: {
-    passwordLength: 'პაროლის სიგრძე',
-    includeSymbols: 'ჩართეთ სიმბოლოები',
-    includeNumbers: 'ჩართეთ ნომრები',
-    includeLowercaseCharacters: 'ჩართეთ მცირე ასოები',
-    includeUppercaseCharacters: 'ჩართეთ დიდი ასოები',
-    excludeSimilarCharacters:
-      'გამორიცხეთ მსგავსი პერსონაჟები',
-    excludeAmbiguousCharacters:
-      'გამორიცხეთ ორაზროვანი პერსონაჟები',
-    generate: 'Გენერირება',
-  },
-  ar: {
-    passwordLength: 'پاس ورڈ کی لمبائی',
-    includeSymbols: 'علامتیں شامل کریں۔',
-    includeNumbers: 'نمبر شامل کریں۔',
-    includeLowercaseCharacters: 'چھوٹے حروف کو شامل کریں۔',
-    includeUppercaseCharacters: 'بڑے حروف کو شامل کریں۔',
-    excludeSimilarCharacters:
-      'ملتے جلتے حروف کو خارج کریں۔',
-    excludeAmbiguousCharacters: 'مبہم حروف کو خارج کریں۔',
-    generate: 'پیدا کریں۔',
-  },
-  hy: {
-    passwordLength: 'Գաղտնաբառի երկարությունը',
-    includeSymbols: 'Ներառեք խորհրդանիշներ',
-    includeNumbers: 'Ներառեք թվեր',
-    includeLowercaseCharacters: 'Ներառեք փոքրատառ նիշերը',
-    includeUppercaseCharacters: 'Ներառեք մեծատառ նիշերը',
-    excludeSimilarCharacters: 'Բացառեք նմանատիպ կերպարները',
-    excludeAmbiguousCharacters:
-      'Բացառեք երկիմաստ կերպարները',
-    generate: 'ստեղծել',
-  },
-  hi: {
-    passwordLength: 'पारण शब्द लम्बाई',
-    includeSymbols: 'प्रतीकों को शामिल करें',
-    includeNumbers: 'संख्याएं शामिल करें',
-    includeLowercaseCharacters: 'लोअरकेस वर्ण शामिल करें',
-    includeUppercaseCharacters: 'अपरकेस वर्ण शामिल करें',
-    excludeSimilarCharacters: 'समान वर्णों को छोड़ दें',
-    excludeAmbiguousCharacters:
-      'अस्पष्ट वर्णों को छोड़ दें',
-    generate: 'बनाना',
-  },
-  it: {
-    passwordLength: 'Lunghezza della password',
-    includeSymbols: 'Includi simboli',
-    includeNumbers: 'Includi numeri',
-    includeLowercaseCharacters:
-      'Includi caratteri minuscoli',
-    includeUppercaseCharacters:
-      'Includi caratteri maiuscoli',
-    excludeSimilarCharacters: 'Escludi caratteri simili',
-    excludeAmbiguousCharacters: 'Escludi caratteri ambigui',
-    generate: 'creare',
-  },
-};
+export const Translations: DeepReadonly<TranslationsType> =
+  {
+    en: {
+      passwordLength: 'Password Length',
+      includeSymbols: 'Include Symbols',
+      includeNumbers: 'Include Numbers',
+      includeLowercaseCharacters:
+        'Include Lowercase Characters',
+      includeUppercaseCharacters:
+        'Include Uppercase Characters',
+      excludeSimilarCharacters:
+        'Exclude Similar Characters',
+      excludeAmbiguousCharacters:
+        'Exclude Ambiguous Characters',
+      generate: 'Generate',
+    },
+    ru: {
+      passwordLength: 'Длина пароля',
+      includeSymbols: 'Включить символы',
+      includeNumbers: 'Включить числа',
+      includeLowercaseCharacters: 'Включить строчные буквы',
+      includeUppercaseCharacters:
+        'Включить заглавные буквы',
+      excludeSimilarCharacters: 'Исключить похожие символы',
+      excludeAmbiguousCharacters:
+        'Исключить неоднозначные символы',
+      generate: 'Сгенерировать',
+    },
+    zh: {
+      passwordLength: '密碼長度',
+      includeSymbols: '包括符號',
+      includeNumbers: '包括數字',
+      includeLowercaseCharacters: '包括小寫字符',
+      includeUppercaseCharacters: '包括大寫字符',
+      excludeSimilarCharacters: '排除相似字符',
+      excludeAmbiguousCharacters: '排除歧義字符',
+      generate: '產生',
+    },
+    ko: {
+      passwordLength: '비밀번호 길이',
+      includeSymbols: '포함 기호',
+      includeNumbers: '포함 숫자',
+      includeLowercaseCharacters: '소문자 포함',
+      includeUppercaseCharacters: '대문자 포함',
+      excludeSimilarCharacters: '유사한 문자 제외',
+      excludeAmbiguousCharacters: '모호한 문자 제외',
+      generate: '생성하다',
+    },
+    ja: {
+      passwordLength: 'パスワードの長さ',
+      includeSymbols: '記号を含む',
+      includeNumbers: '数字を含む',
+      includeLowercaseCharacters: '小文字を含める',
+      includeUppercaseCharacters: '大文字を含む',
+      excludeSimilarCharacters: '類似文字を除外',
+      excludeAmbiguousCharacters:
+        'あいまいな文字を除外する',
+      generate: '生成',
+    },
+    fr: {
+      passwordLength: 'Longueur du mot de passe',
+      includeSymbols: 'Inclure des symboles',
+      includeNumbers: 'Inclure des nombres',
+      includeLowercaseCharacters:
+        'Inclure des caractères minuscules',
+      includeUppercaseCharacters:
+        'Inclure les caractères majuscules',
+      excludeSimilarCharacters:
+        'Exclure les caractères similaires',
+      excludeAmbiguousCharacters:
+        'Exclure les caractères ambigus',
+      generate: 'Générer',
+    },
+    de: {
+      passwordLength: 'Passwortlänge',
+      includeSymbols: 'Symbole einbeziehen',
+      includeNumbers: 'Zahlen einschließen',
+      includeLowercaseCharacters: 'Enthält Kleinbuchstaben',
+      includeUppercaseCharacters:
+        'Großbuchstaben einschließen',
+      excludeSimilarCharacters:
+        'Schließen Sie ähnliche Zeichen aus',
+      excludeAmbiguousCharacters:
+        'Schließen Sie mehrdeutige Zeichen aus',
+      generate: 'Generieren',
+    },
+    id: {
+      passwordLength: 'Panjang kata sandi',
+      includeSymbols: 'Sertakan simbol',
+      includeNumbers: 'Sertakan angka',
+      includeLowercaseCharacters:
+        'Termasuk karakter huruf kecil',
+      includeUppercaseCharacters:
+        'Termasuk karakter huruf besar',
+      excludeSimilarCharacters:
+        'Kecualikan karakter yang mirip',
+      excludeAmbiguousCharacters:
+        'Kecualikan karakter yang ambigu',
+      generate: 'Menghasilkan',
+    },
+    pl: {
+      passwordLength: 'Długość hasła',
+      includeSymbols: 'Dołącz symbole',
+      includeNumbers: 'Uwzględnij liczby',
+      includeLowercaseCharacters: 'Zawiera małe litery',
+      includeUppercaseCharacters: 'Zawiera wielkie litery',
+      excludeSimilarCharacters: 'Wyklucz podobne znaki',
+      excludeAmbiguousCharacters:
+        'Wyklucz niejednoznaczne znaki',
+      generate: 'Generować',
+    },
+    es: {
+      passwordLength: 'Longitud de la contraseña',
+      includeSymbols: 'Incluir símbolos',
+      includeNumbers: 'Incluir números',
+      includeLowercaseCharacters:
+        'Incluir caracteres en minúscula',
+      includeUppercaseCharacters:
+        'Incluir caracteres en mayúscula',
+      excludeSimilarCharacters:
+        'Excluir caracteres similares',
+      excludeAmbiguousCharacters:
+        'Excluir caracteres ambiguos',
+      generate: 'Generar',
+    },
+    ka: {
+      passwordLength: 'პაროლის სიგრძე',
+      includeSymbols: 'ჩართეთ სიმბოლოები',
+      includeNumbers: 'ჩართეთ ნომრები',
+      includeLowercaseCharacters: 'ჩართეთ მცირე ასოები',
+      includeUppercaseCharacters: 'ჩართეთ დიდი ასოები',
+      excludeSimilarCharacters:
+        'გამორიცხეთ მსგავსი პერსონაჟები',
+      excludeAmbiguousCharacters:
+        'გამორიცხეთ ორაზროვანი პერსონაჟები',
+      generate: 'Გენერირება',
+    },
+    ar: {
+      passwordLength: 'پاس ورڈ کی لمبائی',
+      includeSymbols: 'علامتیں شامل کریں۔',
+      includeNumbers: 'نمبر شامل کریں۔',
+      includeLowercaseCharacters:
+        'چھوٹے حروف کو شامل کریں۔',
+      includeUppercaseCharacters: 'بڑے حروف کو شامل کریں۔',
+      excludeSimilarCharacters:
+        'ملتے جلتے حروف کو خارج کریں۔',
+      excludeAmbiguousCharacters: 'مبہم حروف کو خارج کریں۔',
+      generate: 'پیدا کریں۔',
+    },
+    hy: {
+      passwordLength: 'Գաղտնաբառի երկարությունը',
+      includeSymbols: 'Ներառեք խորհրդանիշներ',
+      includeNumbers: 'Ներառեք թվեր',
+      includeLowercaseCharacters: 'Ներառեք փոքրատառ նիշերը',
+      includeUppercaseCharacters: 'Ներառեք մեծատառ նիշերը',
+      excludeSimilarCharacters:
+        'Բացառեք նմանատիպ կերպարները',
+      excludeAmbiguousCharacters:
+        'Բացառեք երկիմաստ կերպարները',
+      generate: 'ստեղծել',
+    },
+    hi: {
+      passwordLength: 'पारण शब्द लम्बाई',
+      includeSymbols: 'प्रतीकों को शामिल करें',
+      includeNumbers: 'संख्याएं शामिल करें',
+      includeLowercaseCharacters: 'लोअरकेस वर्ण शामिल करें',
+      includeUppercaseCharacters: 'अपरकेस वर्ण शामिल करें',
+      excludeSimilarCharacters: 'समान वर्णों को छोड़ दें',
+      excludeAmbiguousCharacters:
+        'अस्पष्ट वर्णों को छोड़ दें',
+      generate: 'बनाना',
+    },
+    it: {
+      passwordLength: 'Lunghezza della password',
+      includeSymbols: 'Includi simboli',
+      includeNumbers: 'Includi numeri',
+      includeLowercaseCharacters:
+        'Includi caratteri minuscoli',
+      includeUppercaseCharacters:
+        'Includi caratteri maiuscoli',
+      excludeSimilarCharacters: 'Escludi caratteri simili',
+      excludeAmbiguousCharacters:
+        'Escludi caratteri ambigui',
+      generate: 'creare',
+    },
+  };
