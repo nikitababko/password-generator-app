5a2e4937663038a44c1de34eab9873e3d1cbadad
Nikita Babko <niki.babko@gmail.com>
2023-07-10 14:48:08 +0700

    fix: render formItems

--- src/components/Form/TopSection/TopSection.controller.ts
+++ src/components/Form/TopSection/TopSection.controller.ts
@@ -26,7 +26,7 @@ export const useController: UseControllerType = () => {
     [dispatch, state.formItems],
   );
 
-  const isLastActiveItemId = useCallback(() => {
+  const isLastActiveItemId = useMemo(() => {
     const items = state.formItems.filter((formItem) => {
       return formItem.value === true;
     });
@@ -38,11 +38,61 @@ export const useController: UseControllerType = () => {
     return null;
   }, [state.formItems]);
 
+  const formItemsWithOnlyTrue = useMemo(
+    () =>
+      state.formItems.filter((formItem) => {
+        return formItem.value === true;
+      }),
+    [state.formItems],
+  );
+
+  const includeFormItems = useMemo(() => {
+    return formItemsWithOnlyTrue.filter((formItem) => {
+      return formItem.label.includes('include');
+    });
+  }, [formItemsWithOnlyTrue]);
+
+  const excludeFormItems = useMemo(() => {
+    return formItemsWithOnlyTrue.filter((formItem) => {
+      return formItem.label.includes('exclude');
+    });
+  }, [formItemsWithOnlyTrue]);
+
+  const hasExcludeFieldId = useMemo(() => {
+    if (
+      excludeFormItems.length > 0 &&
+      includeFormItems.length === 1
+    ) {
+      const excludeFieldId = formItemsWithOnlyTrue.find(
+        (formItem) => {
+          return formItem.label.includes('exclude');
+        },
+      )?.id;
+
+      if (excludeFieldId) {
+        return formItemsWithOnlyTrue.find((formItem) => {
+          return formItem.id !== excludeFieldId;
+        })?.id;
+      }
+
+      return null;
+    }
+
+    return null;
+  }, [
+    excludeFormItems.length,
+    formItemsWithOnlyTrue,
+    includeFormItems.length,
+  ]);
+
   const isDisabledItem = useCallback<IsDisabledItemType>(
     (formItemId) => {
-      return isLastActiveItemId() === formItemId;
+      return (
+        (isLastActiveItemId || hasExcludeFieldId) ===
+        formItemId
+      );
     },
-    [isLastActiveItemId],
+    [hasExcludeFieldId, isLastActiveItemId],
   );
 
   return {
